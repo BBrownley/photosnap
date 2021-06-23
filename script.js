2e59f8ec56f3181c8e72b337d91e51e229c1303f
BBrownley <brendobrownley@yahoo.com>
2021-06-23 03:34:33 -0400

    Built pricing toggle functionality

--- script.js
+++ script.js
@@ -112,6 +112,8 @@ const storyRenderer = (() => {
     ".main-stories .stories-container"
   );
 
+  if (storyContainer === null) return;
+
   stories.forEach(story => {
     const storyElWrapper = document.createElement("div");
     storyElWrapper.classList.add("story-wrapper", story.cssClassName);
@@ -131,41 +133,74 @@ const storyRenderer = (() => {
       <div class="accent-border"></div>
     `;
 
-    // const storyEl = `
-    //   <div class="story-wrapper ${story.cssClassName}">
-    //     <div class="story">
-    //       <p>${story.date}</p>
-    //       <h3>${story.title}</h3>
-    //       <p>by ${story.author}</p>
-    //       <button class="btn-4">
-    //         <span>Read story</span
-    //         ><img
-    //           src="./assets/shared/desktop/arrow.svg"
-    //           class="svg-arrow"
-    //         />
-    //       </button>
-    //     </div>
-    //     <div class="accent-border"></div>
-    //   </div>
-    // `;
-
     storyContainer.appendChild(storyElWrapper);
   });
 })();
 
-{
-  /* <div class="story-wrapper the-mountains">
-<div class="story">
-  <h3>The Mountains</h3>
-  <p>by John Appleseed</p>
-  <button class="btn-4">
-    <span>Read story</span
-    ><img
-      src="./assets/shared/desktop/arrow.svg"
-      class="svg-arrow"
-    />
-  </button>
-</div>
-<div class="accent-border"></div>
-</div>  */
-}
+const priceToggleHandler = (() => {
+  const toggler = document.querySelector(".toggle");
+  const toggleCircle = document.querySelector(".toggle .circle");
+
+  const monthlyLabel = document.getElementById("monthly");
+  const yearlyLabel = document.getElementById("yearly");
+
+  const basicPriceLabel = document.getElementById("basic-price");
+  const proPriceLabel = document.getElementById("pro-price");
+  const businessPriceLabel = document.getElementById("business-price");
+
+  const timespanLabels = document.querySelectorAll(".pricing-timespan");
+
+  let currentState = "monthly";
+
+  const prices = {
+    monthly: {
+      basic: "$19.00",
+      pro: "$39.00",
+      business: "$99.00"
+    },
+    yearly: {
+      basic: "$190.00",
+      pro: "$390.00",
+      business: "$990.00"
+    }
+  };
+
+  const updateLabels = () => {
+    Array.from(timespanLabels).forEach(label => {
+      if (currentState === "monthly") {
+        label.textContent = "month";
+      } else {
+        label.textContent = "year";
+      }
+    });
+
+    if (currentState === "monthly") {
+      basicPriceLabel.textContent = prices.monthly.basic;
+      proPriceLabel.textContent = prices.monthly.pro;
+      businessPriceLabel.textContent = prices.monthly.business;
+    } else {
+      basicPriceLabel.textContent = prices.yearly.basic;
+      proPriceLabel.textContent = prices.yearly.pro;
+      businessPriceLabel.textContent = prices.yearly.business;
+    }
+  };
+
+  toggler.addEventListener("click", () => {
+    // Toggle between monthly and yearly prices
+    toggleCircle.classList.toggle("yearly");
+
+    if (toggleCircle.classList.contains("yearly")) {
+      yearlyLabel.classList.add("active");
+      monthlyLabel.classList.remove("active");
+      currentState = "yearly";
+      updateLabels();
+    } else {
+      monthlyLabel.classList.add("active");
+      yearlyLabel.classList.remove("active");
+      currentState = "monthly";
+      updateLabels();
+    }
+  });
+
+  updateLabels();
+})();
