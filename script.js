3c34a5d2c285b7a786adaeed5a1b591c536978f9
BBrownley <brendobrownley@yahoo.com>
2021-06-24 22:40:41 -0400

    Added additional close functionality to mobile nav menu

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const mobileMenuHandler = (() => {
   const menuIcon = document.querySelector(".header .menu-icon");
   const menu = document.querySelector(".mobile-menu");
+  const menuCloseEl = document.querySelector(".mobile-menu .close");
 
   menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("opened");
     menu.classList.toggle("opened");
+  });
+
+  menuCloseEl.addEventListener("click", () => {
+    menuIcon.classList.remove("opened");
+    menu.classList.remove("opened");
   });
 })();
 
